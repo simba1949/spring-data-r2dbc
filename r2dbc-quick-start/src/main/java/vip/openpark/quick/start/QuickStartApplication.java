aba2953aaf0f6e1c148daec92f99f7ef197d2c94
anthony <anthony1949@zto.com>
2024-02-18 18:01:08 +0800

    feature：基于 spring boot 配置 r2dbc

--- r2dbc-quick-start/src/main/java/vip/openpark/quick/start/QuickStartApplication.java
+++ r2dbc-quick-start/src/main/java/vip/openpark/quick/start/QuickStartApplication.java
@@ -1,47 +1,35 @@
 package vip.openpark.quick.start;
 
-import io.r2dbc.spi.ConnectionFactories;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.ConnectionFactoryOptions;
-import io.r2dbc.spi.Option;
-import reactor.core.publisher.Mono;
-
-import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcTransactionManagerAutoConfiguration;
+import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 
 /**
+ * <div>
+ *     spring boot 对 r2dbc 的自动配置：
+ *     1. {@link R2dbcAutoConfiguration}：主要是配置连接工厂、连接池等；
+ *     2. {@link R2dbcDataAutoConfiguration}：操作数据库响应式客户端，提供 CRUD 操作；
+ *     3. {@link R2dbcRepositoriesAutoConfiguration}：开启 spring data 声明式接口方式的 CRUD；
+ *     4. {@link R2dbcTransactionManagerAutoConfiguration}：事务管理
+ * </div>
+ * <div>
+ *     <title>{@link R2dbcDataAutoConfiguration}</title>
+ *     {@link R2dbcEntityTemplate}：操作数据库响应式客户端，提供 CRUD API
+ *     数据库类型映射关系、转换器、自定义 {@link R2dbcCustomConversions} 转换组件；
+ *     数据类型转换：如 int/Integer/varchar/string/datetime/instant
+ * </div>
+ *
  * @author anthony
- * @version 2024/2/18 16:52
+ * @version 2024/2/18 17:39
  */
+@SpringBootApplication
 public class QuickStartApplication {
-	public static void main(String[] args) throws IOException {
-		ConnectionFactoryOptions options =
-			ConnectionFactoryOptions.builder()
-				.option(ConnectionFactoryOptions.PROTOCOL, "r2dbc")
-				.option(ConnectionFactoryOptions.DRIVER, "mysql")
-				.option(ConnectionFactoryOptions.HOST, "172.17.35.120")
-				.option(ConnectionFactoryOptions.PORT, 3306)
-				.option(ConnectionFactoryOptions.DATABASE, "example")
-				.option(Option.valueOf("serverTimezone"), "Asia/Shanghai")
-				.option(ConnectionFactoryOptions.USER, "root")
-				.option(ConnectionFactoryOptions.PASSWORD, "123456")
-				.build();
-		ConnectionFactory connectionFactory = ConnectionFactories.get(options);
-		
-		Mono.from(connectionFactory.create())
-			.flatMapMany(
-				connection ->
-					connection
-						.createStatement("SELECT * FROM user WHERE username = ?")
-						.bind(0, "LiBai")
-						.execute())
-			.flatMap(
-				result ->
-					// 获取结果集
-					result.map((row, rowMetadata) -> row.get("real_name", String.class)))
-			.doOnNext(System.out::println)
-			.subscribe();
-		
-		// 防止主线程结束
-		System.in.read();
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartApplication.class, args);
 	}
 }
